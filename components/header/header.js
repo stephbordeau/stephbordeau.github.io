5bb03a624f3a19a89aad385df3ced1d6748f8bbb
Eric Bordeau <ebordeau@gmail.com>
2020-03-10 07:34:10 -0400

    Don't use custom element for header

--- components/header/header.js
+++ components/header/header.js
@@ -1,68 +1,52 @@
-class Header extends HTMLElement {
-    links = [{
-        label: 'Services',
-        href: 'services.html'
-    },{
-        label: 'Arvigo® Therapy',
-        href: 'arvigo.html'
-    },{
-        label: 'Yoga',
-        href: 'yoga.html'
-    },{
-        label: 'About',
-        href: 'about.html'
-    },{
-        label: 'Schedule',
-        href: 'schedule.html'
-    }];
+const links = [{
+    label: 'Services',
+    href: 'services.html'
+},{
+    label: 'Arvigo&reg; Therapy',
+    href: 'arvigo.html'
+},{
+    label: 'Yoga',
+    href: 'yoga.html'
+},{
+    label: 'About',
+    href: 'about.html'
+},{
+    label: 'Schedule',
+    href: 'schedule.html'
+}];
 
-    constructor() {
-        super();
-        const shadow = this.attachShadow({mode: 'open'});
-        const [currentPage] = window.location.pathname.split('/').slice(-1);
+const [currentPage] = window.location.pathname.split('/').slice(-1);
 
-        const header = document.createElement('header');
-        const menuIcon = document.createElement('div');
-        menuIcon.classList.add('menu-icon');
-        menuIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 52 52"><path fill="#000" d="m46.5 14h-41c-0.8 0-1.5-0.7-1.5-1.5v-3c0-0.8 0.7-1.5 1.5-1.5h41c0.8 0 1.5 0.7 1.5 1.5v3c0 0.8-0.7 1.5-1.5 1.5z m0 14.9h-41c-0.8 0-1.5-0.7-1.5-1.5v-3c0-0.7 0.7-1.4 1.5-1.4h41c0.8 0 1.5 0.7 1.5 1.5v3c0 0.7-0.7 1.4-1.5 1.4z m0 15.1h-41c-0.8 0-1.5-0.7-1.5-1.5v-3c0-0.8 0.7-1.5 1.5-1.5h41c0.8 0 1.5 0.7 1.5 1.5v3c0 0.8-0.7 1.5-1.5 1.5z"></path></svg>';
-        menuIcon.onclick = () => {
-            this.toggleMenu();
-        };
+const header = document.createElement('header');
+const menuIcon = document.createElement('div');
+menuIcon.classList.add('menu-icon');
+menuIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 52 52"><path fill="#000" d="m46.5 14h-41c-0.8 0-1.5-0.7-1.5-1.5v-3c0-0.8 0.7-1.5 1.5-1.5h41c0.8 0 1.5 0.7 1.5 1.5v3c0 0.8-0.7 1.5-1.5 1.5z m0 14.9h-41c-0.8 0-1.5-0.7-1.5-1.5v-3c0-0.7 0.7-1.4 1.5-1.4h41c0.8 0 1.5 0.7 1.5 1.5v3c0 0.7-0.7 1.4-1.5 1.4z m0 15.1h-41c-0.8 0-1.5-0.7-1.5-1.5v-3c0-0.8 0.7-1.5 1.5-1.5h41c0.8 0 1.5 0.7 1.5 1.5v3c0 0.8-0.7 1.5-1.5 1.5z"></path></svg>';
+menuIcon.onclick = () => {
+    const menu = document.querySelector('.mobile-nav .menu');
+    menu.classList.toggle('visible');
+};
 
-        const title = document.createElement('div');
-        title.classList.add('title');
-        title.textContent = 'Steph Bordeau Massage';
+const title = document.createElement('div');
+title.classList.add('title');
+title.textContent = 'Steph Bordeau Massage';
 
-        header.appendChild(menuIcon);
-        header.appendChild(title);
+header.appendChild(menuIcon);
+header.appendChild(title);
 
-        const menu = document.createElement('nav');
-        menu.classList.add('menu');
-        const links = document.createElement('ul');
-        menu.appendChild(links);
+const menu = document.createElement('nav');
+menu.classList.add('menu');
+const ul = document.createElement('ul');
+menu.appendChild(ul);
 
-        this.links.forEach(link => {
-            const item = document.createElement('li');
-            item.innerHTML = `<a href="${link.href}">${link.label}</a>`;
-            if (currentPage === link.href) {
-                item.classList.add('selected');
-            }
-            links.appendChild(item);
-        });
-
-        const link = document.createElement('link');
-        link.setAttribute('rel', 'stylesheet');
-        link.setAttribute('href', 'components/header/header.css');
-
-        shadow.appendChild(link);
-        shadow.appendChild(header);
-        shadow.appendChild(menu);
+links.forEach(link => {
+    const li = document.createElement('li');
+    li.innerHTML = `<a href="${link.href}">${link.label}</a>`;
+    if (currentPage === link.href) {
+        li.classList.add('selected');
     }
+    ul.appendChild(li);
+});
 
-    toggleMenu() {
-        const menu = this.shadowRoot.querySelector('.menu');
-        menu.classList.toggle('visible');
-    }
-}
-
-customElements.define('sbm-header', Header);
+const parent = document.querySelector('.mobile-nav');
+parent.appendChild(header);
+parent.appendChild(menu);
